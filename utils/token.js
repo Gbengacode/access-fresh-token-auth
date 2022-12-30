b577d69685571499c615625532e355cec7d342b6
emmanuel <gbengacode>
2022-12-30 10:46:11 +0100

    Add refresh and access token

--- utils/token.js
+++ utils/token.js
@@ -2,9 +2,16 @@ import dotenv from 'dotenv'
 dotenv.config()
 import jwt from 'jsonwebtoken'
 
-export const generateAccessToken = (id) => {
-    return jwt.sign({id}, process.env.ACCESS_TOKEN_SECRET)
+export const generateTokens = (res, userInfo) => {
+    const accessToken = jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '300s'})
+    const refreshToken =  jwt.sign(userInfo, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d'})
+    res.cookie('jwt', refreshToken, {
+        httpOnly: true, 
+        secure: true,
+        sameSite: 'None', 
+        maxAge: 7 * 24 * 60 * 60 * 1000 
+    })
+    return accessToken
 }
-export const sendAccessToken = (res, token, result  ) => {
-    return res.json({ token, result })
-}
+
+
